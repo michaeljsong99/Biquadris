52dd2ded32038ab73769eeca6f5e5cabd8e05ddd
michaeljsong99 <44045529+michaeljsong99@users.noreply.github.com>
2018-11-28 21:07:00 -0500

    Update block.cc

--- block.cc
+++ block.cc
@@ -2,6 +2,34 @@
 #include "block.h"
 
 std::ostream& ::operator<<(std::ostream &out, const Block &b){
-    
+    for (int i = 0; i < container_height; ++i) {
+        for (int j = 0; j < container_width; ++j) {
+            cout << container[i][j];
+        }
+        cout << endl;
+    }
+    return out;
 }
 
+/*
+class Block {
+  std::string name; // name of the block
+  <vector <vector Cell>> container;
+  int container_width;
+  int container_height;
+  int levelCreated;
+ public:
+  int getWidth();
+  int getHeight();
+  void setWidth();
+  void setHeight();
+  void setBlock();
+  void moveLeft();
+  void moveRight();
+  void moveDown();
+  void rotateCW();
+  void rotateCCW();
+  void getScore();
+  Block(std::string name, int levelCreated);
+};
+*/
