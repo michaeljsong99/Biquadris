52499512e7168ac79d1309aa390047631540dfc3
michaeljsong99 <44045529+michaeljsong99@users.noreply.github.com>
2018-11-28 21:41:59 -0500

    Update block.cc

--- block.cc
+++ block.cc
@@ -1,7 +1,7 @@
 
 #include "block.h"
 
-std::ostream& ::operator<<(std::ostream &out, const Block &b){
+std::ostream& operator<<(std::ostream &out, const Block &b){
     for (int i = 0; i < b.container_height; ++i) {
         for (int j = 0; j < b.container_width; ++j) {
             cout << b.container[i][j];
