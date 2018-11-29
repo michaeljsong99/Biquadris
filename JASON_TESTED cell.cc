eb0b7981eb250e1dabf5cd316c49c9e32b4d5ec0
SinanGu <35501849+SinanGu@users.noreply.github.com>
2018-11-28 23:22:18 -0500

    Add files via upload

---
+++ JASON_TESTED cell.cc
@@ -0,0 +1,35 @@
+#include "cell.h"
+using namespace std;
+
+string Cell::getLetter() {
+  return letter;
+}
+
+void Cell::setLetter(string alphabet) {
+  letter = alphabet;
+}
+
+int Cell::getX() {
+  return x;
+}
+
+int Cell::getY() {
+  return y;
+}
+
+void Cell::setX(int n) {
+   x = n;
+}
+
+void Cell::setY(int m) {
+   y = m;
+}
+
+ostream &operator<<(ostream &out, Cell &c) {
+   if (c.letter == "") {
+     out << " ";
+     return out;
+   } 
+   out << c.letter;
+   return out;
+}
