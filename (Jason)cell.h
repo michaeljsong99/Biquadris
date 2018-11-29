869d59fedba90888ac4e98f11b5325aa68dc9e3a
SinanGu <35501849+SinanGu@users.noreply.github.com>
2018-11-28 20:34:56 -0500

    Add files via upload

---
+++ (Jason)cell.h
@@ -0,0 +1,16 @@
+#ifndef _CELL_H
+#define _CELL_H
+#include <string>
+class Cell {
+  int x;
+  int y;
+  std::string letter;
+ public:
+  std::string getLetter();
+  void setLetter(std::string alphabet);
+  void setX(int n);
+  void setY(int m);
+  int getX();
+  int getY();
+};
+#endif
