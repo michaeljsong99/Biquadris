2959d2ed3fcaf36510c88c495003c0e8fd1117db
michaeljsong99 <44045529+michaeljsong99@users.noreply.github.com>
2018-12-01 13:50:53 -0500

    Update heavy.cc

--- heavy.cc
+++ heavy.cc
@@ -1,2 +1,25 @@
 
 #include "heavy.h"
+
+class Heavy: public Decorator {
+private:
+    int heavy;
+
+public:
+
+    Heavy(std::shared_ptr<AbstractBoard> ab);
+    // Every time a block moves left or right, it automatically falls by two rows, after the horizontal move.
+    // If it is not possible for the block to drop two rows, it is considered to be dropped, and the turn ends.
+    // If the player is in a level where the blocks are heavy, the effect is cumulative (the player suffers from both effects).
+  
+    bool moveLeft(std::shared_ptr<Block> b) override;           //Returns true if the block doesn't collide
+        
+    bool moveRight(std::shared_ptr<Block> b) override;
+
+    bool moveDown(std::shared_ptr<Block> b) override;
+
+    bool rotateCW(std::shared_ptr<Block> b) override;      //Returns true if the block doesn't collide
+
+    bool rotateCCW(std::shared_ptr<Block> b) override;    //Returns true if the block doesn't collide
+
+};
