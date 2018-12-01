c897765aa05b982003154cef844086d4f4eea998
SinanGu <35501849+SinanGu@users.noreply.github.com>
2018-12-01 13:13:27 -0500

    Update decorator.cc

--- decorator.cc
+++ decorator.cc
@@ -1,2 +1,44 @@
 
 #include "decorator.h"
+#include <memory>
+using namespace std;
+
+bool Decorator::moveLeft(shared_ptr<Block> b) {
+   return component.moveLeft(b);
+}
+
+bool Decorator::moveRight(shared_ptr<Block> b) {
+   return component.moveRight(b);
+}
+
+bool Decorator::moveDown(shared_ptr<Block> b) {
+   return component.moveDown(b);
+}
+
+bool Decorator::rotateCW(shared_ptr<Block> b) {
+   return component.rotateCW(b);
+}
+
+bool Decorator::rotateCCW(shared_ptr<Block> b) {
+   return component.rotateCCW(b);
+}
+
+void Decorator::reset() {
+   return component.reset();
+}
+
+int Decorator::getLevel() {
+   return component.getLevel();
+}
+
+int Decorator::getScore() {
+   return component.getScore();
+}
+
+string Decorator::outputBoard() {
+   return component.outputBoard();
+}
+
+void Decorator::drop() {
+   component.drop();
+}
