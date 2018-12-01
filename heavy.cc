f2a0420c372c72f54d5289424e2785610d8a9a90
michaeljsong99 <44045529+michaeljsong99@users.noreply.github.com>
2018-12-01 13:53:36 -0500

    Update heavy.cc

--- heavy.cc
+++ heavy.cc
@@ -13,7 +13,7 @@ public:
     // If the player is in a level where the blocks are heavy, the effect is cumulative (the player suffers from both effects).
   
     bool moveLeft(std::shared_ptr<Block> b) override;           //Returns true if the block doesn't collide
-        
+         if
     bool moveRight(std::shared_ptr<Block> b) override;
 
     bool moveDown(std::shared_ptr<Block> b) override;
@@ -23,3 +23,39 @@ public:
     bool rotateCCW(std::shared_ptr<Block> b) override;    //Returns true if the block doesn't collide
 
 };
+
+
+
+public:
+
+    void drop() override;
+
+    bool moveLeft(std::shared_ptr<Block> b) override;          //Returns true if the block doesn't collide                  //Stores the blocks as pointers
+    
+    bool CanClearOneRow();
+    
+    int CalculateScore();          //Checks for empty blocks and other score sources
+
+
+    int ClearRows();
+
+    void update();                  //Called after the end of a turn, calls clearRow and calculateScore
+	
+
+
+
+    bool rotateCCW(std::shared_ptr<Block> b) override;
+
+    void reset() override;                  //Resets the board
+
+    std::string outputBoard() override;                  //Prints board as a string
+
+    void changeLevel(int change) override;             //Modify the level by change levels e.g change = -2 level 4->2
+
+    int getLevel() override;                //Returns current Level
+
+    int getScore() override;                //Returns current Level
+
+
+    void updateGrid();
+
