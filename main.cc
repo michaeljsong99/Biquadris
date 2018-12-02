2fcc7dee3e6c8d411015bb5a6ff995a0306cd531
SinanGu <35501849+SinanGu@users.noreply.github.com>
2018-12-02 10:40:29 -0500

    Update main.cc

--- main.cc
+++ main.cc
@@ -1,19 +1,32 @@
 #include <iostream>
+#include <sstream>
 #include <string>
-#include "grid.h"
-#include "cell.h"
+#include "game.h"
 
 using namespace std;
 
 int main (int argc, char *argv[]) {
+  Game game; // call the game constructor
   int seed;
   for (int i = 1; i < argc; i = i + 1) {
-     if (argv[0] = "-text") {
-        // suggestion: probably add a field in the game class to control whether the graphic are shown
-     } else if (argv[0] = "-seed") {
-        seed = argv[i];
+     if (argv[i] = "-text") {
+        // suggestion: add a boolean field in the game class to control whether the graphic are shown
+     } else if (argv[i] = "-seed") {
+        istringstream readStr{argv[i+1]}; // since argv[i+1] is a string
+        readStr >> seed;
         i += 1;
-        // suggestion: probably need to modify a random number 
+        // suggestion: add a field the game class to store the seed
+     }  else if (argv[i] = "-scriptfile1") {
+        game.setFilename1(argv[i+1]);
+        i += 1;
+     }  else if (argv[i] = "-scriptfile2") {
+        game.setFilename2(argv[i+1]);
+        i += 1;
+     }  else if  (argv[i] = "-startLevel") {
+        int level;
+        istringstream readStr{arg[i+1]};
+        readStr >> level;
+        //game.setLevel(level);
      }
   } 
 }
